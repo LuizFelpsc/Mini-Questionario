5a3469afe8ef7290ad985dd3acbfdfd2947f09ba
LuizFelpsc <117685654+LuizFelpsc@users.noreply.github.com>
2022-11-09 11:38:31 -0300

    Add files via upload

---
+++ mini-questionario.py
@@ -0,0 +1,6 @@
+nome= input("Qual e o Seu Nome?")
+idade= int(input("Prazer em Te Conhecer {} Quantos Anos Voce Tem?".format(nome)))
+peso= float(input("Quanto Você Pesa?"))
+estado= input("Qual Estado Voce Mora?")
+print("Voce Se Chama {} Tem {} Anos, Pesa {} e Mora no Estado de {}".format(nome,idade,peso,estado))
+
